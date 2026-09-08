15638eb60d35f504b5d86f6df8c104f16c57aa32
Bahom II <thebahomain@gmail.com>
2026-09-08 21:49:53 +0100

    Use current typedRoutes configuration

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,7 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = { experimental: { typedRoutes: true } };
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+};
+
 export default nextConfig;
